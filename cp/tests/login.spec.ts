9b63328a30d6301cd165e01fd65cb3eb2c670513
anatudose <ana.tudose@softwaremind.com>
2025-03-31 15:54:11 +0300

    await

--- cp/tests/login.spec.ts
+++ cp/tests/login.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Dashboard Redirect", () => {
   test("should see company dashboard after log in", async ({ page }) => {
-    page.goto("/");
+    await page.goto("/");
     const result = page.getByRole("heading", { name: "Dashboard" });
     await expect(result).not.toBeEmpty();
   });
