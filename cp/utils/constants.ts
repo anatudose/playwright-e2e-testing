7c9e6aace59b83c88e3c29bcf578e906c8f4b15b
anatudose <anattudose@gmail.com>
2025-05-16 10:45:56 +0300

    customs page

---
+++ cp/utils/constants.ts
@@ -0,0 +1,38 @@
+export const customsTable = {
+  description: {
+    columnName: "description",
+    columnIndex: 0,
+  },
+  asin: {
+    columnName: "asin",
+    columnIndex: 1,
+  },
+  hsCode: {
+    columnName: "hsCode",
+    columnIndex: 2,
+  },
+  origin: {
+    columnName: "origin",
+    columnIndex: 3,
+  },
+  quantity: {
+    columnName: "quantity",
+    columnIndex: 4,
+  },
+  netWeight: {
+    columnName: "netWeight",
+    columnIndex: 5,
+  },
+  grossWeight: {
+    columnName: "grossWeight",
+    columnIndex: 6,
+  },
+  unitCost: {
+    columnName: "unitCost",
+    columnIndex: 7,
+  },
+  totalCost: {
+    columnName: "totalCost",
+    columnIndex: 8,
+  },
+};
